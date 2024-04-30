314927bd3e5a9cc449df3eab7771b9c47c4bfde1
Stephen Onnen <stephen.onnen@gmail.com>
2024-04-29 19:25:55 -0500

    it works

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 css:
-	./tailwindcss -i ./src/input.css -o ./dist/styles.css
+	./tailwindcss -i ./src/input.css -o ./dist/styles.css --watch
+
+css-watch:
+	./tailwindcss -i ./src/input.css -o ./dist/styles.css --watch
 
 build:
 	elm make src/Main.elm --output=dist/main.js
+
+live:
+	elm-live src/Main.elm --pushstate --startpage=./dist/index.html --dir=./dist -- --output=dist/main.js
--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,10 @@
 module Main exposing (main)
 
-import Browser
-import Html exposing (button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (div, h1, text)
+import Html.Attributes exposing (..)
 
 
-main : Program () Int Msg
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
-
-
-type Msg
-    = Increment
-    | Decrement
-
-
-update : Msg -> number -> number
-update msg model =
-    case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
-
-
-view : Int -> Html.Html Msg
-view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+    div
+        []
+        [ h1 [ class "font-mono text-8xl text-red-700" ] [ text "./onn.sh" ] ]
